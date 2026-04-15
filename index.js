419c4a23d5a3ce4f3afe6e5bfce36a10bd2d7e59
kenleyryan-cell <kenley.ryan@students.msswarriors.org>
2026-04-15 18:12:13 +0000

    check

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstName = 'koko bidies'
+let jobTitle = 'fast food worker'
+let city = 'springfield'
+
+let newJobPost = `dear ${firstName},
+Thank you for being interest in this ${jobTitle} in ${city}`
+
+document.getElementById("result").innerHTML = newJobPost;
